81d2d49880a8b2aeaa1fb497ad998a5f833780d8
rubinashaik886 <rubina.shaik@cdpipelinedevshop.com>
2022-07-27 16:17:47 +0000

    devshop page

---
+++ facebookpage/src/App.js
@@ -0,0 +1,12 @@
+import MainPage from './Components/MainPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
